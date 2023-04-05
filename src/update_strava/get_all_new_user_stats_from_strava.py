2b009ab64538003b53774a7cc0162ab3fe602299
Thomas Kok <thomas.kok@uniklinik-freiburg.de>
2023-04-05 11:44:53 +0200

    updated get_all_new_user_stats_from_strava, now only counts tallied activities

--- src/update_strava/get_all_new_user_stats_from_strava.py
+++ src/update_strava/get_all_new_user_stats_from_strava.py
@@ -1,4 +1,4 @@
-def get_all_new_user_stats_from_strava(user_dict, strava_client_id, strava_client_secret, start_datetime, end_datetime):
+def get_all_user_stats_from_strava(user_dict, strava_client_id, strava_client_secret, start_datetime, end_datetime):
     import time
     from src.update_strava.get_user_stats_from_strava import get_user_activities_from_strava
 
@@ -13,19 +13,23 @@ def get_all_new_user_stats_from_strava(user_dict, strava_client_id, strava_clien
 
         user_activities_df = get_user_activities_from_strava(user_id, strava_client_id, strava_client_secret, start_datetime, end_datetime)
 
-        if not user_activities_df.empty:
-            run_sum_in_m = user_activities_df.loc[user_activities_df['type'] == 'Run', 'distance'].sum()
+        # Filter user_activities for only interested activities (i.e.: running, cycling, walking, and swimming)
+        # Use of '|' instead of 'or' due to need of bitwise operation, else overload
+        user_activities_filtered_df = user_activities_df[(user_activities_df['type'] == 'Run') | (user_activities_df['type'] == 'Ride') | (user_activities_df['type'] == 'Walk') | (user_activities_df['type'] == 'Swim')]
+
+        if not user_activities_filtered_df.empty:
+            run_sum_in_m = user_activities_filtered_df.loc[user_activities_filtered_df['type'] == 'Run', 'distance'].sum()
             tally_dict[user]['Run'] = run_sum_in_m / 1000
-            ride_sum_in_m = user_activities_df.loc[user_activities_df['type'] == 'Ride', 'distance'].sum()
+            ride_sum_in_m = user_activities_filtered_df.loc[user_activities_filtered_df['type'] == 'Ride', 'distance'].sum()
             tally_dict[user]['Ride'] = ride_sum_in_m / 1000
-            walk_sum_in_m = user_activities_df.loc[user_activities_df['type'] == 'Walk', 'distance'].sum()
+            walk_sum_in_m = user_activities_filtered_df.loc[user_activities_filtered_df['type'] == 'Walk', 'distance'].sum()
             tally_dict[user]['Walk'] = walk_sum_in_m / 1000
-            swim_sum_in_m = user_activities_df.loc[user_activities_df['type'] == 'Swim', 'distance'].sum()
+            swim_sum_in_m = user_activities_filtered_df.loc[user_activities_filtered_df['type'] == 'Swim', 'distance'].sum()
             tally_dict[user]['Swim'] = swim_sum_in_m / 1000
-            tally_dict[user]['Cumulative Elevation'] = user_activities_df['total_elevation_gain'].sum()
-            moving_time_in_s = user_activities_df['moving_time'].sum()
+            tally_dict[user]['Cumulative Elevation'] = user_activities_filtered_df['total_elevation_gain'].sum()
+            moving_time_in_s = user_activities_filtered_df['moving_time'].sum()
             tally_dict[user]['Cumulative Time'] = time.strftime('%H:%M:%S', time.gmtime(moving_time_in_s))
-            tally_dict[user]['Number of Activities'] = user_activities_df.shape[0]
+            tally_dict[user]['Number of Activities'] = user_activities_filtered_df.shape[0]
 
         else:
             tally_dict[user]['Run'] = 0
