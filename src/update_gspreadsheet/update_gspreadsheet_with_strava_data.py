cf7fae28bc51e5eb0b7d55d57ad486dcc2ddc58c
Thomas Kok <thomas.kok@uniklinik-freiburg.de>
2023-04-05 11:43:40 +0200

    update_gworksheet_with_strava_data now uses activity_dict from main and fstrings

--- src/update_gspreadsheet/update_gspreadsheet_with_strava_data.py
+++ src/update_gspreadsheet/update_gspreadsheet_with_strava_data.py
@@ -1,13 +1,5 @@
-def update_gworksheet_with_strava_data(worksheet, update_dict, activity_dict):
+def update_gworksheet_with_strava_data(worksheet, update_dict, dict_activity_cols):
     import pandas
-
-    run_col = 'D'
-    cycle_col = 'F'
-    walk_col = 'G'
-    swim_col = 'I'
-    elevation_col = 'L'
-    time_col = 'M'
-    num_of_activities_col = 'N'
 
     start_row = 4
 
@@ -16,21 +8,12 @@ def update_gworksheet_with_strava_data(worksheet, update_dict, activity_dict):
 
     # Update columns
     print("Updating sheet")
-    run_update = worksheet.update(f"{run_col}{start_row}:{run_col}{start_row + update_df.shape[0] - 1}",
-                                  [[val] for val in update_df["Run"].tolist()])
-    cycle_update = worksheet.update(f"{cycle_col}{start_row}:{cycle_col}{start_row + update_df.shape[0] - 1}",
-                                    [[val] for val in update_df["Ride"].tolist()])
-    walk_update = worksheet.update(f"{walk_col}{start_row}:{walk_col}{start_row + update_df.shape[0] - 1}",
-                                   [[val] for val in update_df["Walk"].tolist()])
-    swim_update = worksheet.update(f"{swim_col}{start_row}:{swim_col}{start_row + update_df.shape[0] - 1}",
-                                   [[val] for val in update_df["Swim"].tolist()])
-    elevation_update = worksheet.update(
-        f"{elevation_col}{start_row}:{elevation_col}{start_row + update_df.shape[0] - 1}",
-        [[val] for val in update_df["Cumulative Elevation"].tolist()])
-    time_update = worksheet.update(f"{time_col}{start_row}:{time_col}{start_row + update_df.shape[0] - 1}",
-                                   [[val] for val in update_df["Cumulative Time"].tolist()])
-    num_activities_update = worksheet.update(
-        f"{num_of_activities_col}{start_row}:{num_of_activities_col}{start_row + update_df.shape[0] - 1}",
-        [[val] for val in update_df["Number of Activities"].tolist()])
+    run_update = worksheet.update(f"{dict_activity_cols['Run']}{start_row}:{dict_activity_cols['Run']}{start_row + update_df.shape[0] - 1}", [[val] for val in update_df["Run"].tolist()])
+    cycle_update = worksheet.update(f"{dict_activity_cols['Ride']}{start_row}:{dict_activity_cols['Ride']}{start_row + update_df.shape[0] - 1}", [[val] for val in update_df["Ride"].tolist()])
+    walk_update = worksheet.update(f"{dict_activity_cols['Walk']}{start_row}:{dict_activity_cols['Walk']}{start_row + update_df.shape[0] - 1}", [[val] for val in update_df["Walk"].tolist()])
+    swim_update = worksheet.update(f"{dict_activity_cols['Swim']}{start_row}:{dict_activity_cols['Swim']}{start_row + update_df.shape[0] - 1}", [[val] for val in update_df["Swim"].tolist()])
+    elevation_update = worksheet.update( f"{dict_activity_cols['Cumulative Elevation']}{start_row}:{dict_activity_cols['Cumulative Elevation']}{start_row + update_df.shape[0] - 1}", [[val] for val in update_df["Cumulative Elevation"].tolist()])
+    time_update = worksheet.update(f"{dict_activity_cols['Cumulative Time']}{start_row}:{dict_activity_cols['Cumulative Time']}{start_row + update_df.shape[0] - 1}", [[val] for val in update_df["Cumulative Time"].tolist()])
+    num_activities_update = worksheet.update(f"{dict_activity_cols['Number of Activities']}{start_row}:{dict_activity_cols['Number of Activities']}{start_row + update_df.shape[0] - 1}", [[val] for val in update_df["Number of Activities"].tolist()])
 
     return
