46f959827cd94fc4a6972f2d7876e95b72f86037
Thomas Kok <thomas.kok@uniklinik-freiburg.de>
2023-04-18 09:36:19 +0200

    refactored a la PEP8

--- src/update_strava/get_all_new_user_stats_from_strava.py
+++ src/update_strava/get_all_new_user_stats_from_strava.py
@@ -11,23 +11,35 @@ def get_all_user_stats_from_strava(user_dict, strava_client_id, strava_client_se
         # Get the stats for each user
         tally_dict[user] = {}
 
-        user_activities_df = get_user_activities_from_strava(user_id, strava_client_id, strava_client_secret, start_datetime, end_datetime)
+        user_activities_df = get_user_activities_from_strava(user_id, strava_client_id, strava_client_secret,
+                                                             start_datetime, end_datetime)
 
         # Filter user_activities for only interested activities (i.e.: running, cycling, walking, and swimming)
         # Use of '|' instead of 'or' due to need of bitwise operation, else overload
-        user_activities_filtered_df = user_activities_df[(user_activities_df['type'] == 'Run') | (user_activities_df['type'] == 'Virtual Run') | (user_activities_df['type'] == 'Hike') | (user_activities_df['type'] == 'Ride') | (user_activities_df['type'] == 'Virtual Ride') | (user_activities_df['type'] == 'Walk') | (user_activities_df['type'] == 'Swim')]
+        user_activities_filtered_df = user_activities_df[
+            (user_activities_df['type'] == 'Run') | (user_activities_df['type'] == 'Virtual Run') | (
+                        user_activities_df['type'] == 'Hike') | (user_activities_df['type'] == 'Ride') | (
+                        user_activities_df['type'] == 'Virtual Ride') | (user_activities_df['type'] == 'Walk') | (
+                        user_activities_df['type'] == 'Swim')]
 
         if not user_activities_filtered_df.empty:
-            run_sum_in_m = user_activities_filtered_df.loc[user_activities_filtered_df['type'] == 'Run', 'distance'].sum()
-            virtual_run_sum_in_m = user_activities_filtered_df.loc[user_activities_filtered_df['type'] == 'Virtual Run', 'distance'].sum()
+            run_sum_in_m = user_activities_filtered_df.loc[
+                user_activities_filtered_df['type'] == 'Run', 'distance'].sum()
+            virtual_run_sum_in_m = user_activities_filtered_df.loc[
+                user_activities_filtered_df['type'] == 'Virtual Run', 'distance'].sum()
             tally_dict[user]['Run'] = (run_sum_in_m + virtual_run_sum_in_m) / 1000
-            ride_sum_in_m = user_activities_filtered_df.loc[user_activities_filtered_df['type'] == 'Ride', 'distance'].sum()
-            virtual_ride_sum_in_m = user_activities_filtered_df.loc[user_activities_filtered_df['type'] == 'Virtual Ride', 'distance'].sum()
+            ride_sum_in_m = user_activities_filtered_df.loc[
+                user_activities_filtered_df['type'] == 'Ride', 'distance'].sum()
+            virtual_ride_sum_in_m = user_activities_filtered_df.loc[
+                user_activities_filtered_df['type'] == 'Virtual Ride', 'distance'].sum()
             tally_dict[user]['Ride'] = (ride_sum_in_m + virtual_ride_sum_in_m) / 1000
-            walk_sum_in_m = user_activities_filtered_df.loc[user_activities_filtered_df['type'] == 'Walk', 'distance'].sum()
-            hike_sum_in_m = user_activities_filtered_df.loc[user_activities_filtered_df['type'] == 'Hike', 'distance'].sum()
+            walk_sum_in_m = user_activities_filtered_df.loc[
+                user_activities_filtered_df['type'] == 'Walk', 'distance'].sum()
+            hike_sum_in_m = user_activities_filtered_df.loc[
+                user_activities_filtered_df['type'] == 'Hike', 'distance'].sum()
             tally_dict[user]['Walk'] = (walk_sum_in_m + hike_sum_in_m) / 1000
-            swim_sum_in_m = user_activities_filtered_df.loc[user_activities_filtered_df['type'] == 'Swim', 'distance'].sum()
+            swim_sum_in_m = user_activities_filtered_df.loc[
+                user_activities_filtered_df['type'] == 'Swim', 'distance'].sum()
             tally_dict[user]['Swim'] = swim_sum_in_m / 1000
             tally_dict[user]['Cumulative Elevation'] = user_activities_filtered_df['total_elevation_gain'].sum()
             moving_time_in_s = user_activities_filtered_df['moving_time'].sum()
@@ -44,5 +56,3 @@ def get_all_user_stats_from_strava(user_dict, strava_client_id, strava_client_se
             tally_dict[user]['Number of Activities'] = 0
 
     return tally_dict
-
-
