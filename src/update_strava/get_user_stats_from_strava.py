88e11d753fa0c8021bb58eb1bdf1971ad8333f14
Thomas Kok <thomas.kok@uniklinik-freiburg.de>
2023-05-23 11:18:02 +0200

    Improved status_code, upped number_of_activities_to_parse

--- src/update_strava/get_user_stats_from_strava.py
+++ src/update_strava/get_user_stats_from_strava.py
@@ -46,48 +46,55 @@ def get_user_activities_from_strava(athlete_id, strava_client_id, strava_client_
         strava_token = new_strava_token
 
     # Get page of activities from Strava
-    number_of_activities_to_parse = 200
+    number_of_activities_to_parse = 300
     r = requests.get \
         (f"{strava_api_url}?access_token={strava_token['access_token']}&per_page={number_of_activities_to_parse}&page=1")
-    r = r.json()
 
-    user_activities_df = pd.DataFrame(columns=["id",
-                                               "name",
-                                               "start_date_local",
-                                               "type",
-                                               "distance",
-                                               "moving_time",
-                                               "elapsed_time",
-                                               "total_elevation_gain",
-                                               "private"
-                                               ]
-                                      )
+    # If OK status received
+    if r.status_code == 200:
+        r = r.json()
+        user_activities_df = pd.DataFrame(columns=["id",
+                                                   "name",
+                                                   "start_date_local",
+                                                   "type",
+                                                   "distance",
+                                                   "moving_time",
+                                                   "elapsed_time",
+                                                   "total_elevation_gain",
+                                                   "private"
+                                                   ]
+                                          )
 
-    # if r['message'] == 'Authorization Error':
-    #     pass
+        # if r['message'] == 'Authorization Error':
+        #     continue
 
-    for x in range(len(r)):
-        # Skip if activity is private
-        if r[x]['private'] is True:
-            pass
-        else:
-            # Check if moving time is at least half of the elapsed time
-            moving_time = r[x]['moving_time']
-            elapsed_time = r[x]['elapsed_time']
-            start_date_local = r[x]['start_date_local']
-            if (moving_time <= elapsed_time) and (moving_time >= (0.5 * elapsed_time)) and \
-                    (start_datetime <= start_date_local < end_datetime):
-                user_activities_df.loc[x, 'id'] = r[x]['id']
-                user_activities_df.loc[x, 'name'] = r[x]['name']
-                user_activities_df.loc[x, 'start_date_local'] = r[x]['start_date_local']
-                user_activities_df.loc[x, 'type'] = r[x]['type']
-                user_activities_df.loc[x, 'distance'] = r[x]['distance']
-                user_activities_df.loc[x, 'moving_time'] = r[x]['moving_time']
-                user_activities_df.loc[x, 'elapsed_time'] = r[x]['elapsed_time']
-                user_activities_df.loc[x, 'total_elevation_gain'] = r[x]['total_elevation_gain']
+        for x in range(len(r)):
+            # Skip if activity is private
+            if r[x]['private'] is True:
+                pass
+            else:
+                # Check if moving time is at least half of the elapsed time
+                moving_time = r[x]['moving_time']
+                elapsed_time = r[x]['elapsed_time']
+                start_date_local = r[x]['start_date_local']
+                if (moving_time <= elapsed_time) and (moving_time >= (0.5 * elapsed_time)) and \
+                        (start_datetime <= start_date_local < end_datetime):
+                    user_activities_df.loc[x, 'id'] = r[x]['id']
+                    user_activities_df.loc[x, 'name'] = r[x]['name']
+                    user_activities_df.loc[x, 'start_date_local'] = r[x]['start_date_local']
+                    user_activities_df.loc[x, 'type'] = r[x]['type']
+                    user_activities_df.loc[x, 'distance'] = r[x]['distance']
+                    user_activities_df.loc[x, 'moving_time'] = r[x]['moving_time']
+                    user_activities_df.loc[x, 'elapsed_time'] = r[x]['elapsed_time']
+                    user_activities_df.loc[x, 'total_elevation_gain'] = r[x]['total_elevation_gain']
 
-    # Check if user_activities_df are nearing the number_of_activities_to_parse limit
-    if user_activities_df.shape[0] + 50 > number_of_activities_to_parse:
-        print("Need to raise the number of activities to parse in get_user_stats_from_strava")
+        # Check if user_activities_df are nearing the number_of_activities_to_parse limit
+        number_of_activities_in_user_activities_df = user_activities_df.shape[0]
+        if number_of_activities_in_user_activities_df + 20 > number_of_activities_to_parse:
+            print(f"Athlete {athlete_id}'s user_activities_df length has {number_of_activities_in_user_activities_df}, "
+                  f"time to increase number_of_activities_to_parse!")
 
-    return user_activities_df
+        return user_activities_df
+
+    else:
+        print(f"Problem with athlete {athlete_id}!")
